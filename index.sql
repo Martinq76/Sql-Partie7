303493853e0cd1f7171c9d9fef4cabc2f8b136e3
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-14 12:21:24 +0200

    Fin partie7

---
+++ index.sql
@@ -0,0 +1,7 @@
+use webDevelopment;
+
+DELETE FROM languages WHERE tool='HTML';
+
+UPDATE frameworks SET framework='Symfony2' WHERE framework='Symfony';
+
+UPDATE languages SET version='version 5.1' WHERE tool='JavaScript' AND version='version 5';
