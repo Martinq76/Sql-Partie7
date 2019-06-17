46ef1311b1f986a74d1f71ff1622befe235c0d7f
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-17 16:51:27 +0200

    fin partie

--- index.sql
+++ index.sql
@@ -1,7 +1,7 @@
 use webDevelopment;
 
-DELETE FROM languages WHERE tool='HTML';
+DELETE FROM `languages` WHERE `tool`='HTML';
 
-UPDATE frameworks SET framework='Symfony2' WHERE framework='Symfony';
+UPDATE `frameworks` SET `framework`='Symfony2' WHERE `framework`='Symfony';
 
-UPDATE languages SET version='version 5.1' WHERE tool='JavaScript' AND version='version 5';
+UPDATE `languages` SET `version`='version 5.1' WHERE `tool`='JavaScript' AND `version`='version 5';
